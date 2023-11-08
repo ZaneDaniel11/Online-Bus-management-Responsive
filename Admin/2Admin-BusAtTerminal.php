c567585a377e58471ce02b6619292c73f52e41ff
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-11-08 23:45:51 +0800

    Update 2Admin-BusAtTerminal.php

--- Admin/2Admin-BusAtTerminal.php
+++ Admin/2Admin-BusAtTerminal.php
@@ -1,4 +1,16 @@
-
+<?php
+    include 'sidevar.html';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bus at Terminal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style-busInformation.css">
+</head>
 <body>
 <div class="tableContainer">
     <button type="button" class="btn btn-primary"><a style="color: white; text-decoration:none;" href="2BusAtTerminal.php">Add</a></button>
