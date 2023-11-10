58f3359154c28e93f2199e9c765de913606c0d72
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-11-10 23:49:51 +0800

    shesh

--- Admin/3Admin-Management.php
+++ Admin/3Admin-Management.php
@@ -1,4 +1,18 @@
-
+<?php
+     include("sidevar.html");
+     include("Connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Management</title>
+    <link rel="stylesheet" href="style-busInformation.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
 
 <div class="tableContainer">
         <table class="table table-success table-striped">
