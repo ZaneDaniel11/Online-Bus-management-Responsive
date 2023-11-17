fcfa057f5b6d32a1cbad903d740f91decd4a5195
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-11-17 23:54:18 +0800

    Create 5delete.php

---
+++ Admin/5delete.php
@@ -0,0 +1,12 @@
+<?php 
+
+ include 'Connection.php';
+ if(isset($_GET['deleteid']))
+ {
+    $id = $_GET['deleteid'];
+
+    $deleteSql = "DELETE FROM lost_tb WHERE id = $id";
+
+    $result = mysqli_query($conn,$deleteSql);
+
+   
