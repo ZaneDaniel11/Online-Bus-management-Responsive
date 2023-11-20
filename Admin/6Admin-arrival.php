c0a8853720d30033678dbccc602d812cdc4c91a2
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-11-20 23:56:05 +0800

    Update 6Admin-arrival.php

--- Admin/6Admin-arrival.php
+++ Admin/6Admin-arrival.php
@@ -1,4 +1,17 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrival</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style-busInformation.css">
+
+</head>
+<body>
+    
 <div class="tableContainer">
     <button type="button" class="btn btn-primary"><a style="color: white; text-decoration:none;" href="6add.php">Add</a></button>
         <table class="table table-success table-striped">
