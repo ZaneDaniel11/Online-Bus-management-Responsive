b56e9acb9ea917f41ef238759bfa96a0ac5bc194
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-11-24 23:58:55 +0800

    Create admin.php

---
+++ Admin/admin.php
@@ -0,0 +1,22 @@
+
+<body>
+    <div class="containerForm">
+        <div class="containerPicture">
+            <img src="imgaes/adminlogo.jpg" alt="">
+        </div>
+        <p class="adminP">Admin</p>
+
+
+        <form action="admin.php" method="post">
+            <label for="">Username:</label>
+            <input type="text" name="username"><br>
+            <label for="">Password:</label>
+            <input class="pass" type="password" name="password"> <br>
+            <button><a href="Admin-busInformation.php">Log in</a></button>
+        </form>
+
+        <a href="register-admin.php"> <p class="registerp">Register</p></a>
+    </div>
+</body>
+</html>
+
