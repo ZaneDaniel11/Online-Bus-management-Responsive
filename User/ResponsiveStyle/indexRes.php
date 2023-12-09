9b60af13673165e4bb5f63e2850bb12fa5b7d569
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-12-09 14:44:18 +0800

    image

---
+++ User/ResponsiveStyle/indexRes.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Bus Management System</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@500;600;
+                700&family=Kaushan+Script&family=Montserrat&family=Roboto+Slab&family=Roboto:wght@300&display=swap" 
+                rel="stylesheet">  
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <link rel="stylesheet" href="Index.css">
+
+   <script src="woop.js"></script>
+   
+     
+    
+</head>
+       
+<body>
+ 
+    <div class="navbar1">woop</div>
+
+    <div class="bodypic">
+        <img src="bodyPicture1.png" alt="picture">
+    </div>
+    <div class="section2">
+        <p class="p1">SCHEDULE</p>
+        <p class="p2" style="font-family: serif;">Stay inform with  bus schedule with real-time updates.</p>
+    </div>
+
+    <div class="section3">
+        <p class="p3">Live Bus Travel Information</p>
+    </div>
+
+    <div class="tablecontianer">
+
+        
+        <div class="tableButtonContainer">
+            <div class="stambyButtonContainer">
+                <button id="stambyBtn" class="stambyBtn">BUS STAMBY</button>
+            </div>
+
+            <div class="arrivalButtonContainer">
+                <button class="arrivalBtn" id="arrivalBtn">ARRIVALS</button>
+            </div>
+
+            <div class="departureButtonContainer">
+                <button class="departureBtn" id="departureBtn">DEPARTURES</button>
+            </div>
+        </div>
+
+    <div class="tableContainer">
+       
+    </div>
+        <div class="loadToggleContainer">
+           <div><button id="showMoreBtn">Show More</button></div>
+        </div>
+    </div>
+
+
+    </div>
+
+    
+</body>
+</html>
+
+
